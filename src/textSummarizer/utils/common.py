2585f504702afb96c7b2f46d8a795e8c7c0ef41f
DhruvGandhi31 <dhruvvgandhi3110@gmail.com>
2024-07-02 12:00:38 +0530

    resolving issue

--- src/textSummarizer/utils/common.py
+++ src/textSummarizer/utils/common.py
@@ -12,55 +12,58 @@ from typing import Any
 @ensure_annotations
 def read_yaml(path_to_yaml: Path) -> ConfigBox:
     """
-    reads yaml file and returns
+    Reads YAML file and returns a ConfigBox object.
 
-    Args: path_to_yaml: Path like input
+    Args:
+        path_to_yaml (Path): Path to the YAML file.
 
-    Raises: ValueError: if yaml file is empty
+    Raises:
+        ValueError: If the YAML file is empty.
 
-    Returns: ConfigBox: ConfigBox object
-
+    Returns:
+        ConfigBox: ConfigBox object containing the YAML content.
     """
-
     try:
-        with open('path_to_yaml') as yaml_file:
+        with open(path_to_yaml, 'r') as yaml_file:
             content = yaml.safe_load(yaml_file)
             logger.info(f"YAML file read successfully: {path_to_yaml}")
             return ConfigBox(content)
     except BoxValueError:
-        raise ValueError("ya,l file is empty")
+        raise ValueError("YAML file is empty")
     except Exception as e:
         logger.error(f"Error reading YAML file: {path_to_yaml}")
         raise e
 
 
-@ensure_annotations
 def create_directories(path_to_directories: list[Path]) -> None:
     """
-    creates directories if they do not exist
+    Creates directories if they do not exist.
 
-    Args: path_to_directories: list of paths
+    Args:
+        path_to_directories (list[Path]): List of directory paths to create.
 
-    Returns: None
-
+    Returns:
+        None
     """
-
-    for path in path_to_directories:
-        os.makedirs(path, exist_ok=True)
-        if verbose:
-            logger.info(f"Directory created: {path}")
+    try:
+        for path in path_to_directories:
+            os.makedirs(path, exist_ok=True)
+            if verbose:
+                logger.info(f"Directory created: {path}")
+    except Exception as e:
+        raise e
 
 
 @ensure_annotations
 def get_size(path: Path) -> str:
     """
-    returns size of file
+    Returns the size of the file.
 
-    Args: path: Path like input
+    Args:
+        path (Path): Path to the file.
 
-    Returns: str: size of file
-
+    Returns:
+        str: Size of the file in kilobytes.
     """
-
-    size_in_kb = round(os.pth.getsize(path) / 1024)
+    size_in_kb = round(os.path.getsize(path) / 1024)
     return f"~ {size_in_kb} KB"
